40003c8281026e041d1ce9bd614ab8e5dc3d8f1a
vjp-Alejandromm <72406688+vjp-Alejandromm@users.noreply.github.com>
2022-10-19 19:34:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
 	<body>
 		<p>Hola <b>Mundo</b></p>
-		<p style=color:red;>Alejandro Mesa Martín</p>	
+		<p>Alejandro Mesa Martín</p>	
 	</body>
 
 </html>
+
+<style type="text/css">
+  body {
+    color: purple;
+    background-color: #d8da3d }
+  </style>
